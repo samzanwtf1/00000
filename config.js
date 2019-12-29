1c15fc4648524523a994c72d3d8a561db4681b51
Glitch (metro-daily) <none>
2019-12-29 02:09:14 +0000

    🛬🐆 Updated with Glitch

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.twitter = {
+  consumer_key: '7Q048fWtmcnKJDp2FOqOlzUIC',
+  consumer_secret: 'mwsU0b5NjHVGEnzJA7VxtoGWzaBBQPAHgXkuoZvjYXw2hrwJXa',
+  access_token: '1088079265806106624-OGL9EiwN978D7Y0nRNaDzuzfog8PUx',
+  access_token_secret: 'mmc7Dm5gKnGL4z7cvmS2nJUXT4967HD6YdbQI1jXxkgZr'
+}
+
+module.exports = config;
